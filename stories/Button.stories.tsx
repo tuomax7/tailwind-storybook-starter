188e36f2e2537980fb5ec40d5948c467852fc566
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2023-10-04 11:06:54 +0300

    Add Button

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { FiStar, FiArrowRight } from "react-icons/fi";
+import { Figma } from "../src";
+import StoryLayout from "./StoryLayout";
+
+import { Button, ButtonProps } from "../src/Button";
+
+const meta: Meta = {
+  title: "Button",
+  component: Button,
+  parameters: {
+    controls: { expanded: true },
+    design: {
+      type: "figma",
+      url: Figma.Button,
+    },
+  },
+};
+
+export default meta;
+
+interface Props extends ButtonProps {
+  darkMode: boolean;
+}
+
+const StoryButton: Story<Props> = (args) => (
+  <StoryLayout {...args} className="space-y-2">
+    <Button {...args}>Button CTA</Button>
+
+    <Button {...args} LeadingIcon={<FiStar />}>
+      Button CTA
+    </Button>
+
+    <Button {...args} TrailingIcon={<FiArrowRight />}>
+      Button CTA
+    </Button>
+
+    <Button {...args} IconOnly={<FiArrowRight />} />
+  </StoryLayout>
+);
+
+export const Default = StoryButton.bind({});
+
+Default.args = {
+  variant: "primary",
+  size: "md",
+  darkMode: false,
+  disabled: false,
+};
+
+Default.parameters = {
+  controls: { exclude: ["LeadingIcon", "TrailingIcon", "IconOnly"] },
+};
